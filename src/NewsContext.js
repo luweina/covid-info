70836dae5d91a24d49960bec2694c33364f24111
Daniel Na <luweina93@gmail.com>
2020-10-13 20:20:31 -0700

    second

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
-import NewsAPI from "newsapi"
-const newsapi = new NewsAPI('3e8f1a450098461f9c773942158f4f5d');
+
+
 
 export const NewsContext = createContext();
 
@@ -12,8 +12,9 @@ export const NewsContextProvider = (props) => {
   useEffect(() => {
     axios
       .get(
-        `http://newsapi.org/v2/top-headlines?country=ca&category=health&apiKey=${apiKey}`
+        `https://newsapi.org/v2/top-headlines?country=ca&category=health&apiKey=${apiKey}`
       )
+
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
   }, []);
